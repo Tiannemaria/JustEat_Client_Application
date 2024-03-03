029e4cf82e7ecf6713820ebf5c7d37a1c0ba3218
tiannemaria <tiannemay@gmail.com>
2024-03-03 14:21:52 +0000

    Implementing header and user agent

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
-import json
-
 import requests
 
-response = requests.get("https://uk.api.just-eat.io/discovery/uk/restaurants/enriched/bypostcode/EC4M7RF")
+URL = "https://uk.api.just-eat.io/discovery/uk/restaurants/enriched/bypostcode/EC4M7RF"
+userAgent = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"}
 
-if response.status_code == 200:
+response = requests.get(URL, headers=userAgent)
+response_json = response.json()
 
-    print(response.json())
-else:
+print(response_json)
 
-    print(f"Error: {response.status_code}")
+# print(response)
+# if response.status_code == 200:
+#
+#     print(response.json())
+# else:
+#
+#     print(f"Error: {response.status_code}")
 
