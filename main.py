76c359a56b1f3a768f6bca8c56e4327565ffac2b
tiannemaria <tiannemay@gmail.com>
2024-03-05 14:06:29 +0000

    try and except update, for loop update

--- main.py
+++ main.py
@@ -2,40 +2,46 @@ import json
 
 import requests
 
-URL = "https://uk.api.just-eat.io/discovery/uk/restaurants/enriched/bypostcode/EC1V1NR"
+
+#    Creating a variable and assigning to our URL and User Agent
+JustEatAPI = "https://uk.api.just-eat.io/discovery/uk/restaurants/enriched/bypostcode/UB94AU"
 userAgent = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"}
 
-response = requests.get(URL, headers=userAgent)
+#   Creating a variable for our get request
+response = requests.get(JustEatAPI, headers=userAgent)
 
+#   Creating a variable for our response
 response_content = response.json()
-# try:
-#
-#     if str(response.status_code).startswith("2"):
-#         print(json.dumps(response.json(), indent=2))
-#     elif str(response.status_code).startswith("4"):
-#         print("Client side error:", response.status_code)
-#     elif str(response.status_code).startswith("5"):
-#         print("Server side error:", response.status_code)
-#     else:
-#         print("Failed with status code:", response.status_code)
-#
-# except Exception as x:
-#     print("Unexpected error occurred:", x)
 
 
-with open("response.json", "w") as file:
-    json.dump(response_content, file, indent=4)
+#   Try and except used to handle issues with restaurant data (eg - invalid URL)
+try:
+    #   Creating a variable for the 'restaurants' property in our json
+    restaurant_data = response_content["restaurants"]
 
-print("Response written to 'response.json' file.")
+    #   If statement to ensure there is still an output when there are less than 10 in the 'restaurants' property
+    size = 0
+    if len(restaurant_data) >= 10:
+        size = 10
+    else:
+        size = len(restaurant_data)
 
-restaurant_data = response_content["restaurants"]
+    #   For loop to print the first 10 restaurants along with the 4 data points: name, cuisine, rating and address
+    for i in range(0, size):
+        print("Restaurant", i+1, ":")
+        print(" Name:", restaurant_data[i]['name'])
+        print(" Cuisine:", restaurant_data[i]['cuisines'][0]['name'])
+        print(" Star Rating:", restaurant_data[i]['rating']['starRating'])
+        print(" Full Address:", restaurant_data[i]['address'])
+        print("--------------------")
 
-with open("restaurants.json", "w") as file:
-    json.dump(restaurant_data, file, indent=4)
+except:
+    print("Unable to retrieve restaurant information.")
 
-for i in range(0, 10):
-    print("restaurant {}:".format(i+1))
-    print("     name:", restaurant_data[i]['name'])
-    print("     Star rating:", restaurant_data[i]['rating']['starRating'])
-    print("     Postal Code:", restaurant_data[i]['address']['postalCode'])
-    print("     Cuisine:", restaurant_data[i]['cuisines'][0]['name'])
+#   'Finally' message displays regardless of what happens in the try and except
+finally:
+    print("Number of restaurants found within postcode:", size)
+    print("Postcode --> Restaurant scan complete.")
+
+
+
